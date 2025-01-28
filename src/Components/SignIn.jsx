cde60c83b21d9b539c644d3e46f4a0ee19ec743f
ajaymakvana05 <amakavana460@gmail.com>
2025-01-28 23:33:18 +0530

    create signIn page

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import logo from '../images/logo.png'
+import './signin.css'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div>
+      <div className='signIn-Form'>
+        <div className='signIn'>
+          <div className="form-container">
+            <div className="form">
+              <img className='signIn-logo' src={logo} alt="" />
+              <div>
+                <input type="email" name='email' id='email' placeholder='Enter Your Email ' />
+              </div>
+              <div>
+                <input type="password" name='password' id='password' placeholder='Enter Your Password' />
+              </div>
+              <input type="submit" id='submit-btn' value="Sign In" />
+            </div>
+          </div>
+
+          <div className="form2">
+            Don't have an account? <a href="/signup" style={{ color: "#0095f6" }}>Sign Up</a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
