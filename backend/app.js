7d2afb768c2ebf6cccb9360f34667ba96265a05d
ajaymakvana05 <amakavana460@gmail.com>
2025-01-30 21:29:28 +0530

    fix mongodb connected error

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-const data=require('./data')
-const cors=require("cors")
+// const data=require('./data')
+const cors = require("cors")
 const PORT = 3000;
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+require('dotenv').config()
 
 app.use(cors());
 
@@ -10,6 +13,16 @@ app.get('/', (req, res) => {
     res.json(data);
 })
 
+console.log(process.env.MONGO_URL)
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch((err) => {
+        console.log('Error connecting to database', err);
+    }
+    )
 
 
 app.listen(PORT, (req, res) => {
